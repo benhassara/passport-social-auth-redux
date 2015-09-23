0679c9dab588214d6204227aa977e5d96da5aac9
benhassara <bhassara@gmail.com>
2015-09-23 16:57:56 -0600

    twitter auth implemented

--- server/auth/twitter.js
+++ server/auth/twitter.js
@@ -4,4 +4,29 @@ var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
 var init = require('./init');
 
+passport.use(new TwitterStrategy({
+    consumerKey: config.clientID,
+    consumerSecret: config.clientSecret,
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    var newUser = new User({
+      name: profile.username,
+      oauthID: profile.id
+    });
+    var search = {name: newUser.name};
+    var opts = {upsert: true};
+    var update = {name: newUser.name, oauthID: newUser.oauthID};
 
+    User.findOneAndUpdate(search, update, opts, function(err, user) {
+      if (err)
+        return done(err);
+      else
+        return done(null, user);
+    });
+  }
+));
+
+init();
+
+module.exports = passport;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var linkedin = require('../auth/linkedin');
 var github = require('../auth/github');
+var twitter = require('../auth/twitter');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// LinkedIn routes
 router.get('/auth/linkedin',
   linkedin.authenticate('linkedin'));
 
@@ -15,15 +17,25 @@ router.get('/auth/linkedin/callback',
   function(req, res) {
     // auth successful
     res.redirect('/account');
-  });
+});
 
+// GitHub routes
 router.get('/auth/github',
   github.authenticate('github', {scope: ['user:email']}));
 
 router.get('/auth/github/callback',
   github.authenticate('github', {failureRedirect: '/login'}),
   function(req, res) {
-    //auth successful
+    // auth successful
+    res.redirect('/account');
+});
+
+// Twitter routes
+router.get('/auth/twitter', twitter.authenticate('twitter'));
+
+router.get('/auth/twitter/callback',
+  twitter.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
     res.redirect('/account');
   });
 
